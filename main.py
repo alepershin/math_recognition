f04b2af2eaf117fb296bff06cfd2c479eea19686
pershin <alepershin@mail.ru>
2023-12-10 08:09:46 +0400

    Продолжение

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 import streamlit as st
+import cv2 as cv
+import numpy as np
 
-st.title("Проверка письменных работ по математике")
+from tensorflow.keras.models import load_model
+from PIL import Image
+
+st.title('Проверка письменных работ по математике')
+
+filename = st.file_uploader('Load an image', type=['png', 'jpg'])  # Добавление загрузчика файлов
+
+if not filename is None:                       # Выполнение блока, если загружено изображение
+    image = Image.open(filename)
+    st.image(image)
