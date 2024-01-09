5541d7f364cef25c6c0dc421a91ef2d3e6d1dc23
alepershin <alepershin@mail.ru>
2024-01-09 23:29:29 +0400

    add requirements

--- main.py
+++ main.py
@@ -96,7 +96,7 @@ if not filename is None:                       # Выполнение блока
             # Распознавание примера
             prediction = model.predict(digit)
 
-            if max(prediction[0]) < 0.7:
+            if max(prediction[0]) < 0.5:
                 continue
 
             # Получение и вывод индекса самого большого элемента (это номер распознанного символа)
